e712c3024a73e99621c85c802aa758695bfe2633
iamjoydevdas <iamjoydevdas@gmail.com>
2025-05-04 22:30:33 +0100

    first commit

---
+++ src/test/java/com/toteall/locations/LocationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toteall.locations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
